f0d239828647e541582e0d7c23d75b48a5ec0895
AnthonyAlava05 <anthony_alre@hotmail.com>
2024-09-21 03:17:13 -0500

    21 septiembre

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-redirigir").addEventListener("click", function() {
+    window.location.href = "index.html"; // Cambia el nombre a la página de destino que ya creaste
+});
+
+document.getElementById("btn-dedicatoria").addEventListener("click", function() {
+    window.location.href = "dedicatoria.html";
+});
---
+++ script2.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-menu").addEventListener("click", function() {
+    window.location.href = "principal.html"; // Cambia este enlace al menú principal
+});
+
+document.getElementById("btn-redirigir").addEventListener("click", function() {
+    window.location.href = "index.html"; // Cambia este enlace a la otra página que hayas creado
+});
